9e3afeb4b41f26a2bb17f210c8b34bb61f780722
xbiotech <biotech9261@gmail.com>
2023-02-05 11:14:31 +0100

    ADD Slider to adjust grid size

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function createDivs(num){
     }
 }
 createDivs(256);
+
+const slider = document.querySelector("#slider");
+const output = document.querySelector("#output");
+
+output.textContent = slider.value;
+
+slider.oninput = function() {
+  output.textContent = this.value;
+}
+
